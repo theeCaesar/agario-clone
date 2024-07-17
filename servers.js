fc81a59b173e0457e303b4ebe29325518b77bc65
theeCaesar <workthecaesar@gmail.com>
2024-07-17 22:18:33 +0300

    c

--- servers.js
+++ servers.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/public"));
-const expressServer = app.listen(process.env.PORT);
+const expressServer = app.listen(process.env.PORT || 8080);
 const socketio = require("socket.io");
 const io = socketio(expressServer, {
   cors: {
